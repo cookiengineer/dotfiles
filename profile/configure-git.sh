d1143f0a13c6ab1425fe31f91b510cc36ecbf274
Cookie Engineer <cookiengineer@protonmail.ch>
2018-03-31 16:50:55 +0200

    :bug: Fix for git config, now with autosigning

--- profile/configure-git.sh
+++ profile/configure-git.sh
@@ -6,6 +6,8 @@ fi;
 
 
 git config --global user.name "Cookie Engineer";
+git config --global user.signingkey "61F3D706EDE7DDB6C9E41784A78F070057B04FD7";
+git config --global commit.gpgsign true;
 git config --global diff.tool vimdiff;
 git config --global difftool.prompt false;
 git config --global alias.d difftool;
