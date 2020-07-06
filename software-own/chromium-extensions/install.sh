6180c7de2ec2b50974226c09ddafeec6f8c094bd
Cookie Engineer <cookiengineer@protonmail.ch>
2020-07-06 20:26:11 +0200

    :bug: Fix for Chromium Extensions

--- software-own/chromium-extensions/install.sh
+++ software-own/chromium-extensions/install.sh
@@ -3,14 +3,14 @@
 
 _download_crx () {
 
-	name="$1";
-	crx_id="$2";
-	crx_version="74.0";
-	crx_url="https://clients2.google.com/service/update2/crx?response=redirect&prodversion=$crx_version&x=id%3D$crx_id%26installsource%3Dondemand%26uc";
+	url="$1";
+	crx=$(basename $url);
+	file=$(basename "$(dirname $url)");
 
-	echo "$crx_url";
+	ver="83.0";
+	crx_url="https://clients2.google.com/service/update2/crx?response=redirect&prodversion=$ver&acceptformat=crx2,crx3&x=id%3D$crx%26uc";
 
-	curl -sL "$crx_url" > "/home/$USER/Downloads/CRX/$name.crx";
+	wget -O "$file.crx" "$crx_url";
 
 }
 
@@ -20,17 +20,18 @@ if [ ! -d "/home/$USER/Downloads/CRX" ]; then
 fi;
 
 
-if [[ "$(which curl 2>/dev/null)" == "" ]]; then
-	sudo pacman -S --needed --noconfirm curl;
+if [[ "$(which wget 2>/dev/null)" == "" ]]; then
+	sudo pacman -S --needed --noconfirm wget;
 fi;
 
 
-if [[ "$(which curl 2>/dev/null)" != "" ]]; then
+if [[ "$(which wget 2>/dev/null)" != "" ]]; then
 
-	_download_crx "cookie-autodelete" "fhcgjolkccmbidfldomjliifgaodjagh";
-	_download_crx "dark-reader"       "eimadpbcbfnmbkopoojfekhnkhdbieeh";
-	_download_crx "ublock-origin"     "cjpalhdlnbpafiamejdnhcphjbkeiagm";
-	_download_crx "umatrix"           "ogfcmafjalglgifnmanfmnieipoejdcf";
+	cd /home/$USER/Downloads/CRX;
+
+	_download_crx "https://chrome.google.com/webstore/detail/cookie-autodelete/fhcgjolkccmbidfldomjliifgaodjagh";
+	_download_crx "https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm";
+	_download_crx "https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf";
 
 fi;
 
