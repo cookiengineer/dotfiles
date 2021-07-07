86a886178c45119ee806e48fc4288012332107b7
beans-coding <77169146+beans-coding@users.noreply.github.com>
2021-07-08 05:37:43 +0800

    Delete software-own directory

--- software-own/i3-desktop/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed --noconfirm i3-wm i3status dmenu;
-sudo pacman -S --needed --noconfirm brightnessctl blueman network-manager-applet;
-sudo pacman -S --needed --noconfirm nemo nemo-fileroller nemo-image-converter nemo-preview file-roller;
-sudo pacman -S --needed --noconfirm xcursor-breeze;
-
